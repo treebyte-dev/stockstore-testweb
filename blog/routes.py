21b9f67041685bda3b2eeb30db63f51c74959262
alessandro.zampieri <alessandro.zampieri@treebyte.com>
2019-11-26 17:46:33 +0100

    Add page and styles

--- blog/routes.py
+++ blog/routes.py
@@ -4,8 +4,13 @@ from blog import app
 
 @app.route("/")
 def homepage():
-    posts = [{"title":"Primo titolo", "body": "corpo del primo post"  },
+    posts = [{"title": "Primo titolo", "body": "corpo del primo post"},
              {"title": "Secondo titolo", "body": "corpo del secondo post"}
              ]
     some_boolean_flag = True
     return render_template("homepage.html", posts=posts, boolean_flag=some_boolean_flag)
+
+
+@app.route("/about")
+def about():
+    return render_template("about_page.html")
