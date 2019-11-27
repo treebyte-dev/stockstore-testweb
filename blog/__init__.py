61232c590e08bd5d0e1c9f2d5ea4dd41f756cbb3
alessandro.zampieri <alessandro.zampieri@treebyte.com>
2019-11-27 08:42:27 +0100

    add sqlalchemy and migrate for connect database SQlLITE

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
 
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+with app.app_context():
+    if db.engine.url.drivername == 'sqlite':
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
 
 from blog import routes
 
