ea7ff8493c2bcab43a1607f17b6ac5491c1f4cff
alessandro.zampieri <alessandro.zampieri@treebyte.com>
2019-11-29 18:19:35 +0100

    add Loop post in homepage and add login and logout forms

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,16 @@
 from datetime import datetime
-
+from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from blog import db
+from blog import db, login_manager
 
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    # user added = test psw= modificami
+    # admin psw = 123
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.now)
     username = db.Column(db.String(12), unique=True, nullable=False)
@@ -27,8 +32,10 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     title = db.Column(db.String(120), nullable=False)
+    slug = db.Column(db.String(250))
     description = db.Column(db.String(240))
     body = db.Column(db.Text(), nullable=False)
+    image = db.Column(db.String(120))
 
     def __repr__(self):
         return f"Post('{self.id}', '{self.title}')"
--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,9 @@
-from flask import flash, redirect, render_template, url_for
-from flask_login import current_user, login_user, logout_user
-from blog import app
-from blog.forms import LoginForm
+from flask import abort, flash, redirect, render_template, abort, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from blog import db, app
+from blog.forms import LoginForm, PostForm
 from blog.models import Post, User
+from blog.utils import title_slugifier
 
 
 @app.route("/")
@@ -10,10 +11,52 @@ def homepage():
    posts = Post.query.order_by(Post.created_at.desc()).all()
    return render_template("homepage.html", posts=posts)
 
-@app.route("/posts/<int:post_id>")
-def post_detail(post_id):
-    post_instance = Post.query.get_or_404(post_id)
+@app.route("/posts/<string:post_slug>")
+def post_detail(post_slug):
+    post_instance = Post.query.filter_by(slug=post_slug).first_or_404()
     return render_template("post_detail.html", post=post_instance)
+
+@app.route("/create-post", methods=["GET", "POST"])
+@login_required
+def post_create():
+    form = PostForm()
+    if form.validate_on_submit():
+        slug = title_slugifier(form.title.data)
+        new_post = Post(title=form.title.data, body=form.body.data, slug=slug,
+                        description=form.description.data, author=current_user)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('post_detail', post_slug=slug))
+    return render_template("post_editor.html", form=form)
+
+@app.route("/posts/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def post_update(post_id):
+    post_instance = Post.query.get_or_404(post_id)
+    if post_instance.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post_instance.title = form.title.data
+        post_instance.description = form.description.data
+        post_instance.body = form.body.data
+        db.session.commit()
+        return redirect(url_for('post_detail', post_slug=post_instance.slug))
+    elif request.method == "GET":
+        form.title.data = post_instance.title
+        form.description.data = post_instance.description
+        form.body.data = post_instance.body
+    return render_template("post_editor.html", form=form)
+
+@app.route("/posts/<int:post_id>/delete", methods=["POST"])
+@login_required
+def post_delete(post_id):
+    post_instance = Post.query.get_or_404(post_id)
+    if post_instance.author != current_user:
+        abort(403)
+    db.session.delete(post_instance)
+    db.session.commit()
+    return redirect(url_for('homepage'))
 
 @app.route("/about")
 def about():
