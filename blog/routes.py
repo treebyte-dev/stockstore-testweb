ea2b747f1ea70a6c2334847c3bc536a4441eca5d
alessandro.zampieri <alessandro.zampieri@treebyte.com>
2019-12-02 08:34:26 +0100

    add update and delete post + upload photo

--- blog/routes.py
+++ blog/routes.py
@@ -1,20 +1,37 @@
-from flask import abort, flash, redirect, render_template, abort, url_for, request
+from flask import abort, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
+
 from blog import db, app
 from blog.forms import LoginForm, PostForm
 from blog.models import Post, User
-from blog.utils import title_slugifier
+from blog.utils import save_picture, title_slugifier
 
 
 @app.route("/")
 def homepage():
-   posts = Post.query.order_by(Post.created_at.desc()).all()
-   return render_template("homepage.html", posts=posts)
+    page_number = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.created_at.desc()).paginate(page_number, 6, True)
+
+    if posts.has_next:
+        next_page = url_for('homepage', page=posts.next_num)
+    else:
+        next_page = None
+
+    if posts.has_prev:
+        previous_page = url_for('homepage', page=posts.prev_num)
+    else:
+        previous_page = None
+
+    return render_template("homepage.html", posts=posts, current_page=page_number,
+                           next_page=next_page, previous_page=previous_page)
+
+
 
 @app.route("/posts/<string:post_slug>")
 def post_detail(post_slug):
     post_instance = Post.query.filter_by(slug=post_slug).first_or_404()
     return render_template("post_detail.html", post=post_instance)
+
 
 @app.route("/create-post", methods=["GET", "POST"])
 @login_required
@@ -24,10 +41,22 @@ def post_create():
         slug = title_slugifier(form.title.data)
         new_post = Post(title=form.title.data, body=form.body.data, slug=slug,
                         description=form.description.data, author=current_user)
+
+        if form.image.data:
+            try:
+                image = save_picture(form.image.data)
+                new_post.image = image
+            except Exception:
+                db.session.add(new_post)
+                db.session.commit()
+                flash("C'è stato un problema con l'upload dell'immagine. Cambia immagine e riprova.")
+                return redirect(url_for('post_update', post_id=new_post.id))
+
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for('post_detail', post_slug=slug))
     return render_template("post_editor.html", form=form)
+
 
 @app.route("/posts/<int:post_id>/update", methods=["GET", "POST"])
 @login_required
@@ -40,13 +69,26 @@ def post_update(post_id):
         post_instance.title = form.title.data
         post_instance.description = form.description.data
         post_instance.body = form.body.data
+
+        if form.image.data:
+            try:
+                image = save_picture(form.image.data)
+                post_instance.image = image
+            except Exception:
+                db.session.commit()
+                flash("C'è stato un problema con l'upload dell'immagine. Cambia immagine e riprova.")
+                return redirect(url_for('post_update', post_id=post_instance.id))
+
         db.session.commit()
         return redirect(url_for('post_detail', post_slug=post_instance.slug))
     elif request.method == "GET":
         form.title.data = post_instance.title
         form.description.data = post_instance.description
         form.body.data = post_instance.body
-    return render_template("post_editor.html", form=form)
+
+    post_image = post_instance.image or None
+    return render_template("post_editor.html", form=form, post_image=post_image)
+
 
 @app.route("/posts/<int:post_id>/delete", methods=["POST"])
 @login_required
@@ -58,9 +100,14 @@ def post_delete(post_id):
     db.session.commit()
     return redirect(url_for('homepage'))
 
+
 @app.route("/about")
 def about():
     return render_template("about_page.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact_page.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -81,5 +128,3 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('homepage'))
-
-
